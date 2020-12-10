621a7769a8bcc17d20fbe8d7d82651b20383f40b
Anna <anna@MacBook-Pro.local>
2020-12-10 16:47:43 +0100

    df

--- myscript.js
+++ myscript.js
@@ -5,3 +5,16 @@ function helloFunction(target) {
 function getHelloMessage(target) {
     return "Hello " + target + "!";
 }
+function dotsFunction(count) {
+    if (count%2 == 0) {
+document.getElementById("dots").style.color = "green"; } else {
+document.getElementById("dots").style.color = "red"; }
+let dots = "";
+    for (let i = 0; i < count; i++) {
+          dots += ".";
+}
+document.getElementById("dots").innerHTML = dots; }
+
+function add() {
+    let firstNumber = Number(document.getElementById("first-number").value); let secondNumber = Number(document.getElementById("second-number").value); let result = firstNumber + secondNumber;
+    document.getElementById("result").innerHTML = result; }
